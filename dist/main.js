73237e8a1c4ada796f334a33d7e0899886570276
山本修平 <>
2024-03-18 22:38:32 +0900

    1~10の数字をランダムに入れ替える

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+var board = new Array(10);
+var temp, r1, r2;
+for (var i = 0; i < 10; i++) {
+    board[i] = i;
+}
+for (var count = 0; count < 100; count++) {
+    r1 = Math.floor(Math.random() * 10);
+    r2 = Math.floor(Math.random() * 10);
+    temp = board[r1];
+    board[r1] = board[r2];
+    board[r2] = temp;
+}
+alert(board);
